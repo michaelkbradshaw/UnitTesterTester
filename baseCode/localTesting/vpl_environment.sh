35004456b4877965704c7358646b4d847d715407
Michael K. Bradshaw <bradshaw@hanover.edu>
2014-06-12 08:39:38 -0400

    Added testing code for local environments

---
+++ baseCode/localTesting/vpl_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export VPL_LANG=en.utf8
+export VPL_MAXTIME=120
+export VPL_MAXMEMORY=268435456
+export VPL_MAXFILESIZE=4194304
+export VPL_MAXPROCESSES=50
+export VPL_FILEBASEURL=http://moodle-sdk/stable_25file.php/29/
+export VPL_GRADEMIN=0.00000
+export VPL_GRADEMAX=100.00000
+export VPL_COMPILATIONFAILED='The compilation or preparation of execution has failed'
+export VPL_SUBFILE0=unit.cpp
+export VPL_SUBFILES='unit.cpp ' 
